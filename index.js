4984f0a046f4d92c899101a5d3f3217239baf168
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-23 16:53:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 import moment from "moment";
-import readline from "readline";
+import readline from "readline/promises";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-// pakai external package
-function mainDate1(){
-  rl.question('Masukkan Tanggal: ', (answer) => {
-    if(moment(answer,"DD-MM-YYYY").isValid()){
-      console.log(moment(answer, "DD-MM-YYYY").format("DD/MM/YYYY"));
+async function mainDate1(){
+  try {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    const input = await rl.question('Masukkan tanggal : ');
+    if (moment(input,'DD-MM-YYYY').isValid()) {
+      console.log(moment(input,'DD-MM-YYYY').format('DD/MM/YYYY'));
       rl.close();
-    }else{
-      console.log("Format Tanggal Salah!");
-      mainDate1();
     }
-  });
+    else { 
+      let err = new Error("false");
+      rl.close();
+      throw err;
+    }
+  } catch (err) {
+    if(err.message === "false"){
+      console.log("Format tanggal salah");
+    }
+  }
 }
+
 mainDate1();
 
 // tanpa external package
-function mainDate2() {
-  rl.question('Masukkan tanggal : ', function(input) {
-    let temp = new Date(input.split('-').reverse().join('-'));
-    if (temp === 'Invalid Date') {
-      console.log("Format tanggal salah");
-      mainDate2();
-    } else {
-      let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear();
-      console.log(newDate);
-      rl.close();
-    }
-  });
-}
+// function mainDate2() {
+//   rl.question('Masukkan tanggal : ', function(input) {
+//     let temp = new Date(input.split('-').reverse().join('-'));
+//     if (temp === 'Invalid Date') {
+//       console.log("Format tanggal salah");
+//       mainDate2();
+//     } else {
+//       let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear();
+//       console.log(newDate);
+//       rl.close();
+//     }
+//   });
+// }
 
 // mainDate2();
